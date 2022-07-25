a9aecda303fa78c681d39582c12fb508dc8a22c1
chirstiankaikai <ckaikai19@gmail.com>
2022-07-25 11:03:31 -0400

    added db connection

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const db = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+
+
+db.once("open", () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+  });
+});
 
