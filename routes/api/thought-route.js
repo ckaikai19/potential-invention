5af8b03effc4cf922b08af1b8fa31803bb7e1ad8
chirstiankaikai <ckaikai19@gmail.com>
2022-07-25 12:29:51 -0400

    fixed bugs

--- routes/api/thought-route.js
+++ routes/api/thought-route.js
@@ -1,28 +1,26 @@
 const router = require("express").Router();
-const {
-  getAllThought,
-  getThoughtById,
-  createThought,
-  updateThought,
-  deleteThought,
-  addReaction,
-  removeReaction,
-} = require("../../controllers/thoughtController.js");
+const { getAllThought, getThoughtById, createThought, updateThought, deleteThought, addReaction, removeReaction } = require("../../controllers/thoughtController.js");
 
-// /api/thoughts
-router.route("/").get(getAllThought).post(createThought);
+router
+  .route("/")
+  .get(getAllThought)
+  .post(createThought);
 
-// /api/thoughts/:id
 router
   .route("/:id")
   .get(getThoughtById)
   .put(updateThought)
   .delete(deleteThought);
 
-// /api/thoughts/:thoughtId/reactions
-router.route("/:thoughtId/reactions").post(addReaction);
+router
+  .route("/:thoughtId/reactions")
+  .post(addReaction);
 
-// /api/thoughts/:thoughtId/reactions/:reactionId
-router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+router
+  .route("/:thoughtId/reactions/:reactionId")
+  .delete(removeReaction);
+
+
 
 module.exports = router;
--- routes/api/user-route.js
+++ routes/api/user-route.js
@@ -1,13 +1,23 @@
 const router = require("express").Router();
-const {getAllUser,getUserById,createUser,updateUser,deleteUser,addFriend,removeFriend} = require("../../controllers/userContoller");
+const { getAllUser, getUserById, createUser, updateUser, deleteUser, addFriend, removeFriend } = require("../../controllers/userContoller");
 
 
-router.route("/").get(getAllUser).post(createUser);
+router
+    .route("/")
+    .get(getAllUser)
+    .post(createUser);
 
 // /api/users/:id
-router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+router
+    .route("/:id")
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
 
 // /api/users/:userId/friends/:friendId
-router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
+router
+    .route("/:userId/friends/:friendId")
+    .post(addFriend)
+    .delete(removeFriend);
 
 module.exports = router;
